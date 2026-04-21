ab424dcc89e3e806a96571f20eacf0baa91f4a4c
timgrindall <tim.grindall@gmail.com>
2026-04-21 16:18:18 -0700

    added frontend build

--- index.js
+++ index.js
@@ -28,6 +28,7 @@ let persons = [
 
 app.use(express.json())
 app.use(cors())
+app.use(express.static('dist'))
 
 morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
 app.use(morgan(':method :url :status :res[content-length] :response-time ms :body'))
@@ -116,7 +117,7 @@ app.post('/api/persons', (request, response) => {
     // console.log(request.body)
 })
 
-const PORT = 3001
+const PORT = process.env.port || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })

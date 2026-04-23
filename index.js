ac2d10e5c1cada2117cb0c2b94147855715ed9c7
timgrindall <tim.grindall@gmail.com>
2026-04-23 16:46:43 -0700

    added basic support for mongodb

--- index.js
+++ index.js
@@ -1,30 +1,10 @@
+const Person = require('./models/person')
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const app = express()
 
-let persons = [
-    { 
-      "id": "1",
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": "2",
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": "3",
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": "4",
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
+// no more hardcoded persons var
 
 app.use(express.json())
 app.use(cors())
@@ -38,7 +18,9 @@ app.get('/', (request, response) => {
 })
 
 app.get('/api/persons', (request, response) => {
-    response.json(persons)
+    Person.find({}).then(persons => {
+        response.json(persons)
+    })
 })
 
 app.get('/info', (request, response) => {
@@ -117,7 +99,7 @@ app.post('/api/persons', (request, response) => {
     // console.log(request.body)
 })
 
-const PORT = process.env.port || 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
---
+++ models/person.js
@@ -0,0 +1,38 @@
+require('dotenv').config()
+// console.log("ENV:", process.env.MONGODB_URI)
+
+const mongoose = require('mongoose')
+
+
+if (!process.env.MONGODB_URI) {
+    console.log("No MONGO_URI ENV variable!")
+    process.exit(1)
+}
+
+const url = process.env.MONGODB_URI
+
+mongoose.set('strictQuery',false)
+
+mongoose.connect(url, { family: 4 })
+  .then(result => {
+    console.log('connected to MongoDB')
+  })
+  .catch(error => {
+    console.log('error connecting to MongoDB:', error.message)
+  })
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+  id: String
+})
+
+personSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+module.exports = mongoose.model('Person', personSchema)
